9f36041602ba01f28c965381661981d73fc9717a
SpencerShackleton <85321247+SpencerShackleton@users.noreply.github.com>
2021-06-18 20:18:18 -0400

    Problem 28

--- src/Problem24.java
+++
@@ -1,6 +0,0 @@
-public class Problem24 {
-
-    public static void main(String[] args) {
-
-    }
-}
--- src/Problem28.java
+++ src/Problem28.java
@@ -1,6 +1,16 @@
 public class Problem28 {
 
+    //Theres a smarter way to do this but I didn't find out the right equation
     public static void main(String[] args) {
-
+        int SPIRALS = 1001;
+        int num = 1;
+        int sum = -3;
+        for (int spiral = 1; spiral <= SPIRALS; spiral += 2) {
+            for (int n = 0; n < 4; n++) {
+                num += spiral - 1;
+                sum += num;
+            }
+        }
+        System.out.println(sum);
     }
 }
